db634f7426f69430c20945a7ea782599255d473b
Lorena <lor.guedo@gmail.com>
2026-01-23 21:29:19 +0100

    feat; ajout speedinsightd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "./components/header";
 import Footer from "./components/footer"; 
 import { Montserrat } from "next/font/google";
@@ -30,6 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         <Footer />
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
